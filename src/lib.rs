e7539b2b39b2b7a918b66407315d44c458797ebd
Master-Bw3 <bw1jochman@gmail.com>
2023-07-08 19:04:42 -0500

    colored err

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,23 @@
 use std::fs;
 
 use crate::interpreter::interpret;
-mod parser;
-mod patterns;
-mod iota;
-mod interpreter;
-mod pattern_registry;
 mod compiler;
-
+mod interpreter;
+mod iota;
+mod parser;
+mod pattern_registry;
+mod patterns;
 
 pub fn run() {
-    let source = fs::read_to_string("test.hexcasting").expect("Should have been able to read the file");
+    let path = "./test.hexcasting";
+    let source = fs::read_to_string(path).expect("Should have been able to read the file");
 
     let parse_result = parser::parse(&source).unwrap();
 
     let interpreter_result = interpret(parse_result);
 
-    println!("\n result: {:?} \n {:?}", interpreter_result.as_ref().unwrap().stack, interpreter_result.as_ref().unwrap().buffer)
+    match interpreter_result {
+        Ok(result) => println!("\n result: {:?} \n {:?}", result.stack, result.buffer),
+        Err((err, (line, col))) => eprintln!("\x1b[31mError:\x1b[0m {:?}, {path}:{line}:{col}", err),
+    }
 }
