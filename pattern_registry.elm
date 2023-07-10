fd611752ecbc74df926735cfe596811e692b64e3
Master-Bw3 <bw1jochman@gmail.com>
2023-07-09 20:04:04 -0500

    all non-great spell actions implemented

--- pattern_registry.elm
+++
@@ -1,5 +0,0 @@
-    , { signature = "", internalName = "", action = readChronical, displayName = "", outputOptions = [], selectedOutput = Nothing, startDirection = East }
-    , { signature = "", internalName = "", action = writeChronical, displayName = "", outputOptions = [], selectedOutput = Nothing, startDirection = East }
-    , { signature = "", internalName = "", action = makeConstant (Boolean False), displayName = "", outputOptions = [], selectedOutput = Nothing, startDirection = East }
-    , { signature = "", internalName = "", action = makeConstant (Boolean False), displayName = "", outputOptions = [], selectedOutput = Nothing, startDirection = East }
--- bookkeeper
