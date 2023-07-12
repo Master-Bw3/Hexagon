c0b9a23597a73ec39dedc44d54577c537e17e009
Master-Bw3 <bw1jochman@gmail.com>
2023-07-11 23:35:37 -0500

    error message

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let source_path = &args[1];
+    let source_path = &args.get(1).expect("Expected File Path");
     let config_path = &args.get(2);
 
     hexagon::run(source_path, config_path)
