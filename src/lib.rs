570365264973b44ae51febb3bbacd47f1c1ff6a7
Master-Bw3 <bw1jochman@gmail.com>
2023-07-12 09:47:11 -0500

    run arg

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,30 @@
-use std::{fs, env};
+use std::{env, fs};
 
 use crate::interpreter::interpret;
 mod compiler;
 mod interpreter;
 mod iota;
+mod parse_config;
 mod parser;
 mod pattern_registry;
 mod patterns;
-mod parse_config;
 
-use interpreter::{state::State, mishap::Mishap};
-use parse_config::{parse_config, Config};
+use parse_config::parse_config;
 use pattern_registry::{PatternRegistry, PatternRegistryExt};
 
 pub fn run() {
     let args: Vec<String> = env::args().collect();
 
-    let source_path = args.get(1).expect("Expected File Path");
+    let command = args.get(1).expect("Expected command");
+
+    let source_path = args.get(2).expect("Expected File Path");
 
     let default_config_path = "config.toml".to_string();
-    let config_path = args.get(2).unwrap_or(&default_config_path);
+    let config_path = args.get(3).unwrap_or(&default_config_path);
 
     let config = fs::read_to_string(config_path).map(parse_config).ok();
 
     let source = fs::read_to_string(source_path).expect("Should have been able to read the file");
-
 
     let great_spell_sigs = if let Some(conf) = &config.as_ref() {
         conf.great_spell_sigs.clone()
@@ -33,18 +33,19 @@ pub fn run() {
     };
 
     let parse_result = parser::parse(&source, &great_spell_sigs).unwrap();
-    
-    
+    if command == "run" {
+        let interpreter_result = interpret(parse_result, &config.as_ref());
 
-    let interpreter_result = interpret(parse_result, &config.as_ref());
-
-    match interpreter_result {
-        Ok(result) => println!("\n result: {:?} \n {:?}", result.stack, result.buffer),
-        Err((err, (line, col))) => {
-            eprintln!(
-                "\x1b[31mError:\x1b[0m {:?}, {source_path}:{line}:{col}",
-                err
-            )
-        }
-    };
+        match interpreter_result {
+            Ok(result) => println!("\n result: {:?} \n {:?}", result.stack, result.buffer),
+            Err((err, (line, col))) => {
+                eprintln!(
+                    "\x1b[31mError:\x1b[0m {:?}, {source_path}:{line}:{col}",
+                    err
+                )
+            }
+        };
+    } else {
+        todo!("build")
+    }
 }
