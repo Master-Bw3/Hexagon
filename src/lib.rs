e8ab27e2e7f4d1806fa51813e04d8ad6f46a95e0
Master-Bw3 <bw1jochman@gmail.com>
2023-07-12 09:58:50 -0500

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -12,19 +12,50 @@ mod patterns;
 use parse_config::parse_config;
 use pattern_registry::{PatternRegistry, PatternRegistryExt};
 
+struct Args{
+    command: Command,
+    source_path: String,
+    config_path: String,
+}
+
+impl Args{
+    fn get() -> Args{
+        let args: Vec<String> = env::args().collect();
+
+        let command = args.get(1).expect("Expected command");
+
+        let source_path = args.get(2).expect("Expected File Path").to_owned();
+
+        let default_config_path = "config.toml".to_string();
+        let config_path = args.get(3).unwrap_or(&default_config_path).to_owned();
+
+        Args {
+            command: Args::get_cmd(command),
+            source_path,
+            config_path,
+        }
+    }
+
+    fn get_cmd(cmd: &str) -> Command {
+        match cmd {
+            "run" => Command::Run,
+            _ => panic!("invalid command"),
+        }
+    }
+}
+
+enum Command {
+    Run,
+    Build
+}
+
 pub fn run() {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::get();
 
-    let command = args.get(1).expect("Expected command");
+    let config = fs::read_to_string(args.config_path).map(parse_config).ok();
 
-    let source_path = args.get(2).expect("Expected File Path");
-
-    let default_config_path = "config.toml".to_string();
-    let config_path = args.get(3).unwrap_or(&default_config_path);
-
-    let config = fs::read_to_string(config_path).map(parse_config).ok();
-
-    let source = fs::read_to_string(source_path).expect("Should have been able to read the file");
+    let source =
+        fs::read_to_string(&args.source_path).expect("Should have been able to read the file");
 
     let great_spell_sigs = if let Some(conf) = &config.as_ref() {
         conf.great_spell_sigs.clone()
@@ -33,15 +64,16 @@ pub fn run() {
     };
 
     let parse_result = parser::parse(&source, &great_spell_sigs).unwrap();
-    if command == "run" {
+    
+    if let Command::Run = args.command {
         let interpreter_result = interpret(parse_result, &config.as_ref());
 
         match interpreter_result {
             Ok(result) => println!("\n result: {:?} \n {:?}", result.stack, result.buffer),
             Err((err, (line, col))) => {
                 eprintln!(
-                    "\x1b[31mError:\x1b[0m {:?}, {source_path}:{line}:{col}",
-                    err
+                    "\x1b[31mError:\x1b[0m {:?}, {}:{line}:{col}",
+                    err, args.source_path
                 )
             }
         };
