5a72e1b1421c2a0f2954cb687e7bdf90576407c7
Master-Bw3 <bw1jochman@gmail.com>
2023-07-14 12:59:40 -0500

    fix matrix display

--- src/iota.rs
+++ src/iota.rs
@@ -292,6 +292,6 @@ impl Display for MatrixIota {
             let row_str = format!("[{}]", row_out.join(", "));
             out.push(row_str)
         }
-        out.join("\n")
+        format!("[{}]", out.join("\n"))
     }
 }
