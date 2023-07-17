cda9791830767c97ad9eb52dbf21c48db7714d66
Master-Bw3 <bw1jochman@gmail.com>
2023-07-17 11:51:43 -0500

    fix parser

--- src/parser.rs
+++ src/parser.rs
@@ -61,7 +61,6 @@ fn construct_ast_node(
 
             Some(parse_op(name, arg, pattern_registry, conf_entities))
         }
-        Rule::EscapedIntroRetro => Some(parse_intro_retro(pair)),
         Rule::Var => Some(parse_var(pair)),
         Rule::Embed => Some(parse_embed(pair, pattern_registry, conf_entities)),
         Rule::IfBlock => Some(parse_if_block(pair, pattern_registry, conf_entities)),
