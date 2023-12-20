6a078f377d0344c08b93f8c5db5a187014a79cc2
Master-Bw3 <bw1jochman@gmail.com>
2023-12-20 15:04:04 -0600

    mote iota

--- src/iota.rs
+++ src/iota.rs
@@ -2,15 +2,18 @@ use std::fmt::Debug;
 
 use downcast_rs::{impl_downcast, Downcast};
 
-pub mod hex_casting;
-pub mod more_iotas;
 pub mod five_dim_casting;
+pub mod hex_casting;
+pub mod hexal;
+pub mod more_iotas;
 
 pub trait Iota: Downcast + Debug {
     fn display(&self) -> String;
     fn tolerates_other(&self, other: &dyn Iota) -> bool;
     fn serialize_to_nbt(&self) -> String;
-    fn display_type_name() -> String where Self: Sized;
+    fn display_type_name() -> String
+    where
+        Self: Sized;
 }
 impl_downcast!(Iota);
 
---
+++ src/iota/hexal/mote.rs
@@ -0,0 +1,6 @@
+use crate::iota::more_iotas::string;
+
+pub struct mote {
+    pub name: String,
+    pub count: i32,
+}
