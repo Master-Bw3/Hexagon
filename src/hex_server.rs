8afc3675571554207ecd09a482a754f254522aec
Master-Bw3 <bw1jochman@gmail.com>
2024-08-18 17:41:56 -0500

    fix http request

--- src/hex_server.rs
+++ src/hex_server.rs
@@ -1,11 +1,13 @@
 use std::rc::Rc;
 
+use reqwest::header::CONTENT_TYPE;
+
 use crate::{compiler::nbt::compile_nbt, iota::Iota};
 
 pub fn send_hex(iota_list: Vec<Rc<dyn Iota>>, url: &str) -> Result<String, reqwest::Error> {
     let client = reqwest::blocking::Client::new();
-    let content = format!("SNBT={}", compile_nbt(iota_list));
-    let response = client.post(url).body(content).send()?.text()?;
+    let content = [("SNBT", compile_nbt(iota_list))];
+    let response = client.post(url).form(&content).send()?.text()?;
 
     Ok(response)
 }
